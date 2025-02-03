5ebaa4e19796f4613521d307da77694b095da117
Adarsh11234 <aadarshpatil123@gmail.com>
2025-02-03 10:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
 };
 
 // Define the Employee constructor function inheriting from Person
@@ -17,8 +17,9 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+Person.prototype.greet = function() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+};
 };
 
 // Do not change code below this line
